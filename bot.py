c92dbc3e14aefb4fbee5804d93bde39c588bc492
= <themossunder@gmail.com>
2026-04-11 02:07:19 +0300

    fixing...

--- bot.py
+++ bot.py
@@ -2,12 +2,20 @@ import os
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from core.di.injection import load_dependencies
 from handlers import register_handlers
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не задан в переменных окружения")
 
 logging.basicConfig(level=logging.INFO)
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
 register_handlers(dp)
 
 async def main():
@@ -15,3 +23,22 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+# import os
+# import asyncio
+# import logging
+# from aiogram import Bot, Dispatcher
+# from core.di.injection import load_dependencies
+# from handlers import register_handlers
+
+# logging.basicConfig(level=logging.INFO)
+# bot = Bot(token=BOT_TOKEN)
+# dp = Dispatcher()
+# register_handlers(dp)
+
+# async def main():
+#     await dp.start_polling(bot)
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
